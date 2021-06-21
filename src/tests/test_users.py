f695e1416f8a627b34fea9c278760db19a3b76e6
biTcom18 <nabei@bitcom.info>
2021-06-21 13:59:08 +1000

    Ten tests passed

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -1,6 +1,7 @@
 # src/tests/test_users.py
 
 import json
+from src.api.models import User
 
 
 def test_add_users(test_app, test_database):
@@ -16,3 +17,72 @@ def test_add_users(test_app, test_database):
     data = json.loads(resp.data.decode())
     assert resp.status_code == 201
     assert 'nabei@bitcom.info was added' in data['message']
+
+
+def test_add_user_invalid_json(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({}),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Input payload validation failed' in data['message']
+
+
+def test_user_imvalid_json_keys(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/users',
+        data=json.dumps({"email": "nabei@bitcom.info"}),
+        content_type='application/json'
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Input payload validation failed' in data['message']
+
+
+def test_add_user_duplicate_email(test_app, test_database):
+    client = test_app.test_client()
+    client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'ivan',
+            'email': 'nabei@bitcom.info'
+        }),
+        content_type='application/json',
+    )
+    resp = client.post(
+        '/users',
+        data=json.dumps({
+            'username': 'ivan',
+            'email': 'nabei@bitcom.info'
+        }),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert 'Sorry.That email already exists.' in data['message']
+
+
+
+def test_single_user(test_app, test_database, add_user):
+    user = add_user(username='ivan', email='ivan@testdriven.io')
+    client = test_app.test_client()
+    resp = client.get(f'/users/{user.id}')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    assert 'ivan' in data['username']
+    assert 'ivan@testdriven.io' in data['email']
+
+
+def test_single_user_incorrect_id(test_app,test_database):
+    client = test_app.test_client()
+    resp = client.get('/users/999')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 404
+    assert 'User 999 does not exist' in data['message']
+
+
+def test_all_users(test_app, test_database)
